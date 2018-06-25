87925d8391d54d4dcc386c46e8c71c8636198ab8
toufik <toufik@toufik-PC.home>
2018-06-25 21:27:38 +0200

    add correction exo1

---
+++ src/idp/java/Exercice1.java
@@ -0,0 +1,49 @@
+package idp.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercice1 {
+	public static void main(String[] args) {
+		String bonjour = "salut la compagnie";
+		System.out.println(bonjour.toUpperCase());
+		System.out.println(premiereMajuscule(bonjour));
+		System.out.println(premiereMajusculeV2(bonjour));
+		
+	}
+
+	static String premiereMajuscule(String chaine) {
+		String resultat = "";
+		boolean debutMotPossible = true;
+		for (int i = 0; i < chaine.length(); i++) {
+			if (chaine.charAt(i) != ' ' && debutMotPossible) {
+				resultat += Character.toUpperCase(chaine.charAt(i));
+				debutMotPossible = false;
+			} else{
+				resultat += chaine.charAt(i);
+			}
+			if (chaine.charAt(i) == ' '){
+				debutMotPossible = true;
+			}
+		}
+		return resultat;
+	}
+	
+	static String premiereMajusculeV2(String chaine) {
+		String[] chaines = chaine.split(" ");
+		List<String> chainesL = Arrays.asList(chaines);
+		List<String> chainesResulat = new ArrayList<>();
+
+		chainesL.stream().forEach(s -> {
+			chainesResulat.add(premiereMajusculeMot(s));
+		});
+		return String.join(" ", chainesResulat);
+	}
+
+	private static String premiereMajusculeMot(String s) {
+		String result = Character.toUpperCase(s.substring(0, 1).charAt(0)) + s.substring(1);
+		return result;
+	}
+	
+}
