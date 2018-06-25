b3e4276bc19fb51a4c531222895525d46f0a47e9
toufik <toufik@toufik-PC.home>
2018-06-25 21:47:33 +0200

    Update exo 1 add exo 2

--- src/idp/java/Chaine.java
+++ src/idp/java/Chaine.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Exercice1 {
+public class Chaine {
 	public static void main(String[] args) {
 		String bonjour = "salut la compagnie";
 		System.out.println(bonjour.toUpperCase());
---
+++ src/idp/java/InformationsSysteme.java
@@ -0,0 +1,8 @@
+package idp.java;
+
+public class InformationsSysteme {
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("user.name"));
+		System.out.println(System.getProperty("os.name"));
+	}
+}
