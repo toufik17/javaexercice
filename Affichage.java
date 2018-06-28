cffb9154ce59134cd2c650e9ba4b42421413da1d
Maxime Carpentier <37069799+maximecarpentier2@users.noreply.github.com>
2018-06-28 10:07:41 +0200

    Create Affichage.java

---
+++ Affichage.java
@@ -0,0 +1,22 @@
+package employes;
+
+public class Affichage {
+
+	public static void main(String[] args) {
+		
+		Employe maxime = new Employe("Maxime", 24, 10000);
+		maxime.augmentation(359.6);
+		maxime.afficher();
+		
+		System.out.println();
+		
+		Employe martin = new Technicien("Martin", 29, 2500,(byte) 2);
+		martin.augmentation(400);
+		System.out.println("Le salaire avec prime de " + martin.getNom() + " est de : " + martin.calculeSalaire());
+		martin.afficher();  // salaire avant prime
+		martin.setSalaire(martin.calculeSalaire());
+		martin.afficher();  // salaire après primer
+
+	}
+
+}
